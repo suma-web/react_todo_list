84f8b4b9af23cb26a09128ae4cb344e1275bcdea
sample tarou <sample@example.com>
2026-02-28 14:33:07 +0900

    styleからclassNameに変更

--- src/Organisms/Header/Header.jsx
+++ src/Organisms/Header/Header.jsx
@@ -1,18 +1,8 @@
+import styles from "./Header.jsx"
+
 export const Header = () => {
-    const header = {
-        fontSize: "10px",
-        padding: "8px",
-        color: "white",
-        backgroundColor: "green"
-
-    }
-
-    const H1 = {
-        marginLeft: "20px"
-    }
-
     return (
-        <div style={header}>
+        <div className={styles.header}>
             <h1 style={H1}>React Todo List</h1>
         </div>
     )
