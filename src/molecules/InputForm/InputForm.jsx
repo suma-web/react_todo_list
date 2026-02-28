01e077cdd82e3b6ba7988f0a94375a0cd1b8063c
sample tarou <sample@example.com>
2026-02-28 14:25:56 +0900

    <Button /> を<PrimaryButton />に変更

--- src/molecules/InputForm/InputForm.jsx
+++ src/molecules/InputForm/InputForm.jsx
@@ -1,15 +1,19 @@
 import { Input } from "../../atoms/Input/Input"
-import { InputButton } from "../../atoms/Button/Button"
+import { PrimaryButton } from "../../atoms/Button/PromaryButton"
 
-export const InputForm = ({todoContent, setTodoContent, onClickCreateContent}) => {
-    return (
-        <>
-            <Input
-                todoContent={todoContent}
-                setTodoContent={setTodoContent}
-            />
-            {todoContent.length > 0 && 
-                <InputButton onClickCreateContent={onClickCreateContent} />}
-        </>
-    );
-}
+export const InputForm = ({todoContent,setTodoContent,onClickCreateContent,}) => {
+  return (
+    <>
+      <Input
+        value={todoContent}
+        onChange={(e) => setTodoContent(e.target.value)}
+      />
+
+      {todoContent.length > 0 && (
+        <PrimaryButton onClick={onClickCreateContent}>
+          Add
+        </PrimaryButton>
+      )}
+    </>
+  );
+};
