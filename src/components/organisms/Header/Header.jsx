5d91b3e0081f152c9082904e6ec5ec9f4b846910
sample tarou <sample@example.com>
2026-02-28 15:56:53 +0900

    h1タグのclassNameにstyles.を追記

--- src/components/organisms/Header/Header.jsx
+++ src/components/organisms/Header/Header.jsx
@@ -3,7 +3,7 @@ import styles from "./Header.module.css"
 export const Header = () => {
     return (
         <div className={styles.header}>
-            <h1 className={H1}>React Todo List</h1>
+            <h1 className={styles.h1}>React Todo List</h1>
         </div>
     )
 }
