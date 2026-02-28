010c627fff34864c65153e4425da77bb5c201229
sample tarou <sample@example.com>
2026-02-28 15:24:29 +0900

    styleをclassNameに変更

--- src/components/organisms/Header/Header.jsx
+++ src/components/organisms/Header/Header.jsx
@@ -3,7 +3,7 @@ import styles from "./Header.jsx"
 export const Header = () => {
     return (
         <div className={styles.header}>
-            <h1 style={H1}>React Todo List</h1>
+            <h1 className={H1}>React Todo List</h1>
         </div>
     )
 }
