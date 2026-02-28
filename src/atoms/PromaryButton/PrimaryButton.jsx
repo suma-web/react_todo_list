aca8b1c8ce1a1ce9dd765dcc6a60ac9e5419e8b3
sample tarou <sample@example.com>
2026-02-28 14:27:04 +0900

    ファイル名変更によるファイル削除

--- src/atoms/PromaryButton/PrimaryButton.jsx
+++
@@ -1,9 +0,0 @@
-import styles from "./PrimaryButton.module.css";
-
-export const PrimaryButton = ({ children, ...props }) => {
-  return (
-    <button className={styles.button} {...props}>
-      {children}
-    </button>
-  );
-};
