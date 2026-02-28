9f41d29342e63cd133e32374c493369db398d417
sample tarou <sample@example.com>
2026-02-28 15:12:44 +0900

    インポートするファイルの修正

--- src/molecules/InputForm/InputForm.jsx
+++ src/molecules/InputForm/InputForm.jsx
@@ -1,12 +1,12 @@
 import { Input } from "../../atoms/Input/Input"
-import { PrimaryButton } from "../../atoms/Button/PromaryButton"
+import { PrimaryButton } from "../../atoms/Button/PrimaryButton"
 
-export const InputForm = ({todoContent,setTodoContent,onClickCreateContent,}) => {
+export const InputForm = ({todoContent,setTodoContent,onClickCreateContent}) => {
   return (
     <>
       <Input
         value={todoContent}
-        onChange={(e) => setTodoContent(e.target.value)}
+        onChange={setTodoContent}
         placeholder="Here, write a task"
       />
 
