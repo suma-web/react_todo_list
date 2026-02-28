afbedb824eaa0926553852abaf211cd718be24dc
sample tarou <sample@example.com>
2026-02-28 15:08:13 +0900

    引数の追加

--- src/atoms/Input/Input.jsx
+++ src/atoms/Input/Input.jsx
@@ -1,12 +1,13 @@
 import styles from "./Input.module.css";
 
-export const Input = ({value, onChange}) => {
+export const Input = ({value, onChange, placeholder, ...props}) => {
     return (
         <input 
-            style={styles.input}
+            className={styles.input}
             value={value}
             onChange={(e) => onChange(e.target.value) }
-            placeholder="Here, write a task"
+            placeholder={placeholder}
+            {...props}
         />
     );
 
