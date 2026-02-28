a15a0c93bd634581b14f9d986e6f34fe9ed736d1
sample tarou <sample@example.com>
2026-02-28 13:36:57 +0900

    Input/にInput.jsx、Input.module.cssを作成。

--- src/atoms/Input/Input.jsx
+++ src/atoms/Input/Input.jsx
@@ -1,18 +1,11 @@
-export const Input = ({todoContent, setTodoContent}) => {
-    const input = {
-        width: "20%",
-        minWidth: "200px",
-        padding: "10px",
-        borderRadius: "25px",
-        border: "none",
-        boxShadow: "5px 5px 10px rgba(192, 192, 192, 0.5)"
-    }
+import styles from "./Input.module.css";
 
+export const Input = ({value, onChange}) => {
     return (
         <input 
-            style={input}
-            value={todoContent}
-            onChange={(e) => setTodoContent(e.target.value) }
+            style={styles.input}
+            value={value}
+            onChange={(e) => onChange(e.target.value) }
             placeholder="Here, write a task"
         />
     );
